8079c836a730591011af6abdf2eeb08ff5e98819
arthurz <zachary.arthur@lightsource.ca>
2020-10-26 12:56:52 -0600

    Add preprocess & badscans to package base import level

--- sgmdata/__init__.py
+++ sgmdata/__init__.py
@@ -1,4 +1,6 @@
 from sgmdata import *
 from sgmdata.load import SGMData
+from sgmdata.search import preprocess, badscans
+
 
 
--- sgmdata/utilities/__init__.py
+++ sgmdata/utilities/__init__.py
@@ -1,3 +1,3 @@
-from .util import h5tree, scan_health
+from .util import h5tree, scan_health, printTree
 
-__all__ = ['h5tree', 'scan_health']
+__all__ = ['h5tree', 'scan_health', 'printTree']
