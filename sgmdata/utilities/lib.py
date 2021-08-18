ff03683950003f222b698fbfe5aa17baa9cb8ff1
arthurz <Zachary.Arthur@lightsource.ca>
2021-08-18 16:02:26 -0600

    Update lib.py

--- sgmdata/utilities/lib.py
+++ sgmdata/utilities/lib.py
@@ -10,7 +10,7 @@ scan_lib.update({'EEMs': "run_eems 1 544", 'C': "gscan 270 320 col 320", 'N': "g
                  'O': "gscan 520 570 col 544", 'F': "gscan 675 705 col 685", 'Na': "gscan 1060 1110 col 1075",
                  'Mg': "gscan 1300 1350 col 1312", 'Al': "gscan 1550 1610 col 1568", 'Si': "gscan 1835 1875 col 1847",
                  'P': "", 'K': "", 'Ca': "gscan 340 360 col 353", 'Ti': "gscan 450 480 col 461",
-                 'Mn': "gscan 630 670 col 641", 'Fe': "gscan 690 750 col 711", 'Co': "gscan 775 810 col 780",
+                 'Mn': "gscan 625 670 col 638", 'Fe': "gscan 690 750 col 711", 'Co': "gscan 770 810 col 780.5",
                  'Ni': "gscan 840 890 col 852.5", 'Cu': "gscan 920 970 col 934", 'Zn': "gscan 1000 1080 col 1060",
                  'Ge': "", 'Se': "gscan 1420 1500 col 1500",
                  })
