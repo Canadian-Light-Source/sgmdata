6acc906dfdfa9cbe45fefbda20866fdcc880a681
Zachary Arthur <zachary.arthur@lightsource.ca>
2021-09-27 13:42:18 -0600

    OneList woes

--- sgmdata/utilities/magicclass.py
+++ sgmdata/utilities/magicclass.py
@@ -1,8 +1,6 @@
 class OneList(list):
     def __init__(self, iterable, **kwargs):
         self.l = list(iterable)
-        for i in range(0, len(self.l)):
-            self.__setitem__(i, self.l[i])
         if hasattr(self, 'value'):
             class_name = type(self.value)
             dr = dir(class_name)
