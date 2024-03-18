8aeb22b9934c5e3c458b8cee2f39930771702fb7
arthurz <zachary.arthur@lightsource.ca>
2024-03-17 20:07:50 -0600

    Load processed files bugfix

--- sgmdata/load.py
+++ sgmdata/load.py
@@ -185,7 +185,7 @@ class SGMScan(DisplayDict):
                 NXentries = [str(x) for x in h5['/'].keys() if 'NXentry' in str(h5[x].attrs.get('NX_class'))]
             NXdata = [entry + "/" + str(x) for entry in NXentries for x in h5['/' + entry].keys()
                       if 'NXdata' in str(h5[entry + "/" + x].attrs.get('NX_class'))]
-            axes = [[str(nm) for nm in h5[nxdata].keys() for s in h5[nxdata].attrs.get('axes') if str(s) in str(nm) or
+            axes = [[str(nm) for nm in h5[nxdata].keys() for s in h5[nxdata].attrs.get('axes') if str(s) == str(nm) or
                      str(nm) in str(s)] for nxdata in NXdata]
             indep_shape = [v.shape for i, d in enumerate(NXdata) for k, v in h5[d].items() if k in axes[i][0]]
             data = [
@@ -566,7 +566,7 @@ class SGMData(object):
                          if 'NXentry' in str(h5[x].attrs.get('NX_class'))]
             NXdata = [entry + "/" + str(x) for entry in NXentries for x in h5['/' + entry].keys()
                       if 'NXdata' in str(h5[entry + "/" + x].attrs.get('NX_class'))]
-            axes = [[str(nm) for nm in h5[nxdata].keys() for s in h5[nxdata].attrs.get('axes') if str(s) in str(nm) or
+            axes = [[str(nm) for nm in h5[nxdata].keys() for s in h5[nxdata].attrs.get('axes') if str(s) == str(nm) or
                      str(nm) in str(s)] for nxdata in NXdata]
             indep_shape = [v.shape for i, d in enumerate(NXdata) for k, v in h5[d].items() if k in axes[i][0] and hasattr(v, 'shape')]
             index = [pd.DataFrame.from_dict({ax.replace('_processed', ''): h5[d][ax] for ax in axes[i]}) for i, d in enumerate(NXdata)]
